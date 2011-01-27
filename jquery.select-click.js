5c00d5b6d8d95a4f0aa48e791e54542e8b12dc34
Skilldrick <skilldrick@gmail.com>
2011-01-27 11:23:22 +0000

    First commit of selectClick

---
+++ jquery.select-click.js
@@ -0,0 +1,24 @@
+(function ($) {
+  $.fn.selectClick = function (selectCallback, clickCallback, clickTolerance) {
+    var prevX = 0,
+        prevY = 0,
+        clickTolerance = clickTolerance || 2;
+
+    this.mousedown(function (e) {
+      prevX = e.pageX;
+      prevY = e.pageY
+    });
+
+    return this.click(function (e) {
+      var dx = Math.abs(e.pageX - prevX);
+      var dy = Math.abs(e.pageY - prevY);
+      if (dx < clickTolerance && dy < clickTolerance) {
+        clickCallback();
+      }
+      else {
+        selectCallback();
+      }
+    });
+  };
+
+})(jQuery);
